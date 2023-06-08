f640a6e172c9177d0ad1b31cc7f42466c8515e00
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2023-06-08 22:20:49 +0100

    Get sample to build using linker script.

--- samples/hello/src/main.rs
+++ samples/hello/src/main.rs
@@ -4,7 +4,7 @@
 extern crate neotron_sdk;
 
 #[no_mangle]
-extern "C" fn main() -> i32 {
+extern "C" fn neotron_main() -> i32 {
     let stdout = neotron_sdk::stdout();
     stdout.write(b"Hello, world\n").unwrap();
     0
