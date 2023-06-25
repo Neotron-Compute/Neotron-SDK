cde4309f9b0b8ac30c9003f0dc92473dad46da73
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2023-06-25 21:54:14 +0100

    Update build script

--- samples/build.sh
+++ samples/build.sh
@@ -7,5 +7,5 @@ TARGET=${1:-thumbv6m-none-eabi}
 echo "Building for ${TARGET}"
 for program in panic hello fault; do
     ( cd ${program} && cargo build --target=${TARGET} --release )
-    rust-objcopy -O binary ./${program}/target/${TARGET}/release/${program} ${program}.bin
+    cp ./${program}/target/${TARGET}/release/${program} ${program}.elf
 done
