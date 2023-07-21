0705187970249ae4b068472f1f4c5bd885ad8ee4
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-07-21 16:49:24 +0100

    Adds new input test sample program.

---
+++ samples/input_test/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+extern crate neotron_sdk;
+
+use core::fmt::Write;
+
+#[no_mangle]
+extern "C" fn neotron_main() -> i32 {
+    let mut stdout = neotron_sdk::stdout();
+    let stdin = neotron_sdk::stdin();
+    let _ = stdout.write(b"Type some things, press Ctrl-X to quit...\n");
+    loop {
+        let mut buffer = [0u8; 16];
+        match stdin.read(&mut buffer) {
+            Err(_) => {
+                return 1;
+            }
+            Ok(0) => {
+                // Do nothing
+            }
+            Ok(n) => {
+                for b in &buffer[0..n] {
+                    let _ = writeln!(stdout, "0x{:02x}", b);
+                    if *b == 0x18 {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
