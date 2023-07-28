1ee28615460dd54c0bfd5b8f4318e98ad7be4f80
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-07-28 14:04:34 +0100

    Clean up existing build script.

--- samples/build.sh
+++ samples/build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+#
+# Builds all the Neotron SDK sample binaries.
+#
+# Specify the target as the first argument. Defaults to "thumbv6m-none-eabi" if
+# not given.
+#
+# ```console
+# $ ./build.sh thumbv7em-none-eabi
+# $ ls *.elf
+# ```
+#
+
 set -euo pipefail
 
 TARGET=${1:-thumbv6m-none-eabi}
 
+mkdir -p ./release
+
 echo "Building for ${TARGET}"
-for program in panic hello fault; do
+for program in panic hello fault input-test; do
     ( cd ${program} && cargo build --target=${TARGET} --release )
-    cp ./${program}/target/${TARGET}/release/${program} ${program}.elf
+    cp ./${program}/target/${TARGET}/release/${program} ./release/${program}.elf
 done
--- samples/fault/src/main.rs
+++ samples/fault/src/main.rs
@@ -7,7 +7,7 @@ use core::fmt::Write;
 extern "C" fn neotron_main() -> i32 {
     let stdout = neotron_sdk::stdout();
     writeln!(&stdout, "About to fault...\n").unwrap();
-    let bad_address: u32 = 0xDEAD_C0DE;
+    let bad_address: usize = 0xDEAD_C0DE;
     let bad_fn: fn() = unsafe { core::mem::transmute(bad_address) };
     bad_fn();
     0
